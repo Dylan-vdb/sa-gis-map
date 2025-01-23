baa035d1ab135403e28d6fac0fc970753cebe9ba
Dylan <dylanbosch55@gmail.com>
2025-01-23 11:40:44 +0200

    svg example works

--- src/pages/sandpit.vue
+++ src/pages/sandpit.vue
@@ -1,101 +1,86 @@
 <template>
   <div id="app">
     <div id="map"></div>
-    <div id="popup-container" ref="popupContainer"></div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-import PopupComponent from "@/components/MarkerPopup.vue";
-import { createApp, h } from "vue";
-import "ol/ol.css";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import Feature from "ol/Feature";
-import Point from "ol/geom/Point";
-import Overlay from "ol/Overlay";
-import { fromLonLat } from "ol/proj";
+import Feature from "ol/Feature.js";
+import Layer from "ol/layer/Layer.js";
+import Map from "ol/Map.js";
+import VectorSource from "ol/source/Vector.js";
+import View from "ol/View.js";
+import { Point } from "ol/geom.js";
+import { Vector } from "ol/layer.js";
+import { composeCssTransform } from "ol/transform.js";
 
-const myMarkers = [
-  { name: "Marker 1", description: "Description 1", coordinates: [0, 0] },
-  { name: "Marker 2", description: "Description 2", coordinates: [1, 1] },
-  // Add more markers as needed
-];
-
-const map = ref(null);
-const popup = ref(null);
-const popupContainer = ref(null);
-
-function addPopups(PopupComponent, popupContainer, createApp, h) {
-  popup.value = new Overlay({
-    element: popupContainer.value,
-    positioning: "bottom-center",
-    stopEvent: false,
-  });
-
-  map.value.addOverlay(popup.value);
-
-  map.value.on("click", (evt) => {
-    const feature = map.value.forEachFeatureAtPixel(evt.pixel, (feature) => {
-      return feature;
-    });
-    if (feature) {
-      const coordinates = feature.getGeometry().getCoordinates();
-      popup.value.setPosition(coordinates);
-
-      // Render the popup component dynamically
-      const app = createApp({
-        render() {
-          return h(PopupComponent, {
-            name: feature.get("name"),
-            description: feature.get("description"),
-          });
-        },
-      });
-
-      app.mount(popupContainer.value);
-    } else {
-      popup.value.setPosition(undefined);
-    }
-  });
-}
+import worldSvg from "@/assets/world.svg";
 
 onMounted(() => {
-  map.value = new Map({
+  const map = new Map({
     target: "map",
-    layers: [
-      new TileLayer({
-        source: new OSM(),
-      }),
-    ],
     view: new View({
-      center: fromLonLat([0, 0]),
+      center: [0, 0],
+      extent: [-180, -90, 180, 90],
+      projection: "EPSG:4326",
       zoom: 2,
     }),
   });
 
-  const vectorSource = new VectorSource();
-  myMarkers.forEach((marker) => {
-    const feature = new Feature({
-      geometry: new Point(fromLonLat(marker.coordinates)),
-      name: marker.name,
-      description: marker.description,
-    });
-    vectorSource.addFeature(feature);
+  const svgContainer = document.createElement("div");
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", worldSvg);
+  xhr.addEventListener("load", function () {
+    const svg = xhr.responseXML.documentElement;
+    svgContainer.ownerDocument.importNode(svg);
+    svgContainer.appendChild(svg);
   });
+  xhr.send();
 
-  const vectorLayer = new VectorLayer({
-    source: vectorSource,
-  });
+  const width = 2560;
+  const height = 1280;
+  const svgResolution = 360 / width;
+  svgContainer.style.width = width + "px";
+  svgContainer.style.height = height + "px";
+  svgContainer.style.transformOrigin = "top left";
+  svgContainer.className = "svg-layer";
+  svgContainer.style.position = "absolute";
 
-  map.value.addLayer(vectorLayer);
+  map.addLayer(
+    new Layer({
+      render: function (frameState) {
+        const scale = svgResolution / frameState.viewState.resolution;
+        const center = frameState.viewState.center;
+        const size = frameState.size;
+        const cssTransform = composeCssTransform(
+          size[0] / 2,
+          size[1] / 2,
+          scale,
+          scale,
+          frameState.viewState.rotation,
+          -center[0] / svgResolution - width / 2,
+          center[1] / svgResolution - height / 2
+        );
+        svgContainer.style.transform = cssTransform;
+        svgContainer.style.opacity = this.getOpacity();
+        return svgContainer;
+      },
+    })
+  );
 
-  addPopups(PopupComponent, popupContainer, createApp, h);
+  map.addLayer(
+    new Vector({
+      source: new VectorSource({
+        features: [new Feature(new Point([0, 0]))],
+      }),
+      style: {
+        "circle-fill-color": "blue",
+        "circle-radius": 10,
+        "circle-stroke-color": "white",
+      },
+    })
+  );
 });
 </script>
 
@@ -103,9 +88,18 @@ onMounted(() => {
 #map {
   height: 90vh;
   width: 100%;
+  background: black;
 }
 
 #popup-container {
   margin-bottom: 20px;
+}
+
+.svg-layer path:hover {
+  opacity: 0.4;
+}
+
+.ol-layer {
+  pointer-events: none;
 }
 </style>
